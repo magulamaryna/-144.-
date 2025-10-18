043b453723321defe508ecd51cd0614a662f1a0b
raisapodolska91-commits <raisapodolska91@gmail.com>
2025-10-18 08:51:15 +0300

    Add files via upload

---
+++ Raisaitprojer_5.py
@@ -0,0 +1,23 @@
+num = int(input("Enter num: "))
+
+isHerCar = True
+# ==, !=, <, >, <=, >=.
+if num >= 70 or not isHerCar:
+    print("YES")
+    if num == 100:
+        print("Num is 100")
+elif num == 50:
+    print("Num is 50")
+elif num == 69:
+    print("Num is 69")
+elif num < 9:
+    print("Num is less 9")
+else:
+    print("Else")
+
+isCutie = True
+if not isCutie:
+    print("Yes, she is cutie")
+print("MORE TEXT")
+
+
