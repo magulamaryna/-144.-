113a9d9b19de217779b51985011dc1d85215c706
Vitalina-Zavodyak <vitalina20095@gmail.com>
2025-11-24 10:57:07 +0200

    Add files via upload

---
+++ ZAVODYAK_LR10_Z_4.py
@@ -0,0 +1,9 @@
+import math
+
+x = float(input("ведіть х "))
+y = float(input("ведіть y "))
+z = float(input("ведіть z "))
+t = float(input("ведіть t "))
+
+s = x * y + z * t
+print("площа чотирикутника", s)
