b70ab4fb6a1c734a197b20a873d0defb17ceaf38
veronika1104 <valeriakabackova@gmail.com>
2025-12-03 11:39:50 +0200

    Add files via upload

---
+++ LR_8_motrinec.veronika.py
@@ -0,0 +1,16 @@
+import random
+spusok = [
+    [1,-2,3],
+    [4,-5,6],
+    [-7,8,-9]
+]
+print(spusok)
+for i in range(len(spusok)):
+    for j in range(len(spusok[i])):
+        spusok[i][j] =
+        print(spusok)
+
+
+
+
+
