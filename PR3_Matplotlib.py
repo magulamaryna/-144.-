9d796b174df2c90aba3c0b130d12295c238ddfe8
Vitalina-Zavodyak <vitalina20095@gmail.com>
2026-05-17 17:43:37 +0300

    Add files via upload

---
+++ PR3_Matplotlib.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x = [1, 5, 10, 15, 20]
+y1 = [1, 7, 3, 5, 11]
+y2 = [4, 3, 1, 8, 12]
+plt.plot(x, y1, '8-b')
+plt.plot(x, y2, '^-.m')
+plt.legend(['graf1' , 'graf2'])
+plt.show()
+
+
